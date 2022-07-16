c287cace22f1d53bc0b942bb6893ae60b69dee42
KaHcTpykTa <7806034@mail.ru>
2022-07-16 23:50:34 +0300

    carousel add

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -8,44 +8,40 @@ const Carousel = () => {
       img: "https://media.dolcemaster.co.il/products/20220711094122.jpg",
     },
     {
-        ind: 1,
-        img: "https://media.dolcemaster.co.il/products/20220704134718.jpg",
-      },
-      {
-        ind: 2,
-        img: "https://media.dolcemaster.co.il/products/20220713081207.png",
-      },
-      {
-        ind: 3,
-        img: "https://media.dolcemaster.co.il/products/20220705122947.jpg",
-      },
-      {
-        ind: 4,
-        img: "https://media.dolcemaster.co.il/products/20210106132524.jpg",
-      },
-
+      ind: 1,
+      img: "https://media.dolcemaster.co.il/products/20220704134718.jpg",
+    },
+    {
+      ind: 2,
+      img: "https://media.dolcemaster.co.il/products/20220713081207.png",
+    },
+    {
+      ind: 3,
+      img: "https://media.dolcemaster.co.il/products/20220705122947.jpg",
+    },
+    {
+      ind: 4,
+      img: "https://media.dolcemaster.co.il/products/20210106132524.jpg",
+    },
   ];
-  const max = images.length - 1;
-  const [activeIndex, setActiveIndex] = useState(1);
-  /* const [activeIndex, setActiveIndex] = useState(images.length > 2 ? 1 : 0); */
+  const [activeIndex, setActiveIndex] = useState(images.length > 2 ? 1 : 0);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(2);
-  const [image, setImage] = useState(images[1].img);
-  /* const [images, setImages] = useState(data); */
 
-  console.log(start);
-  console.log("activeIndex = " + activeIndex);
-  console.log(end);
-  console.log(
-    images.filter(
-      (el) =>
-        el === images[start] || el === images[activeIndex] || el === images[end]
-    )
-  );
+  const result = [];
+  result.push(images[start]);
+  result.push(images[activeIndex]);
+  result.push(images[end]);
+
+  const max = images.length - 1;
 
   useEffect(() => {
-    setStart(activeIndex - 1);
-    setEnd(activeIndex + 1);
+    setStart(activeIndex === 0 ? max : activeIndex - 1);
+    setEnd(activeIndex === max ? 0 : activeIndex + 1);
+
+    console.log(start);
+    console.log(activeIndex);
+    console.log(end);
   }, [activeIndex]);
 
   return (
@@ -54,38 +50,16 @@ const Carousel = () => {
         <img src={images[activeIndex].img} className="c-main-img" alt="" />
       </div>
       <div className="c-footer">
-        {images
-          .filter(
-            (el) =>
-              el === images[start] ||
-              el === images[activeIndex] ||
-              el === images[end]
-          )
+        {result
           .map((item, index) => (
             <img
-              className={index === activeIndex ? "c-footer-img-active" : "c-footer-img"}
+              className={index === 1 ? "c-footer-img-active" : "c-footer-img"}
               alt=""
               key={index}
               src={item.img}
-              onClick={() => {
-                setActiveIndex(item.ind);
-              }}
+              onClick={() => setActiveIndex(item.ind)}
             />
           ))}
-      </div>
-      <div className="c-footer">
-        {images.map((item, index) => (
-          <img
-            className={index === 1 ? "c-footer-img-active" : "c-footer-img"}
-            alt=""
-            key={index}
-            src={item.img}
-            onClick={() => {
-              setImage(item);
-              setActiveIndex(index);
-            }}
-          />
-        ))}
       </div>
     </div>
   );
