cb9d72049f86e2bd86b0685d0f4883ae27826911
KaHcTpykTa <7806034@mail.ru>
2022-07-17 22:48:57 +0300

    debounce

--- src/components/btnNext/BtnNext.jsx
+++ src/components/btnNext/BtnNext.jsx
@@ -1,12 +1,34 @@
-import React from "react"
-import "./btnNext.css"
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "../../services/debounce";
+import "./btnNext.css";
 
 const BtnNext = () => {
-  return (
-    <div className="b">
-        <div className="b-btn"/>
-    </div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default BtnNext
+  const debounced = useDebounce(value);
+
+  useEffect(() => {
+    console.log(debounced);
+  }, [debounced]);
+
+  return (
+    <>
+      <div className="b">
+        <div className="b-btn" />
+      </div>
+      <div className="b-input">
+        <label htmlFor="input-search">Input</label>
+        <input
+        type="text"
+          name="input-search"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        {/*         <h4>{value}</h4>
+        <h4>{debounced}</h4> */}
+      </div>
+    </>
+  );
+};
+
+export default BtnNext;
